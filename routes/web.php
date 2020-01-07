368f40b00124283c301d6b9bfec6567b79e866ee
Adam M <whiskypo082@gmail.com>
2020-01-07 13:36:59 -0600

    Added styling for looks and feels

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
 });
+
+Route::get('/blog/show', function () {
+    return view('blog.show');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
